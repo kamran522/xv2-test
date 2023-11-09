e6868d4b794775211a8fcb28fde6036bb8699349
kamran522 <107455935+kamran522@users.noreply.github.com>
2023-11-09 12:31:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '',
-SUDO: process.env.SUDO || '917594898804',
-BOT_INFO: process.env.BOT_INFO ||'X-BOT-MD-V2;ASWIN SPARKY',
-STICKER_DATA: process.env.STICKER_DATA || `X-BOT-MD;SPERKY`,
+SESSION_ID: process.env.SESSION_ID || 'X-BOT-MD:ProwlingPlagued',
+SUDO: process.env.SUDO || '923323963788',
+BOT_INFO: process.env.BOT_INFO ||'X-BOT-MD-V2;K A M I',
+STICKER_DATA: process.env.STICKER_DATA || `X-BOT-MD;K A M I`,
 PREFIX: process.env.PREFIX || '.',
 MENTION: 'https://i.imgur.com/SBWolI2.mp4' ,
 MENTION_DATA: 'X-BOT-MD;MULTI-DEVICE BOT BY TEAM EX-BOT-Z;https://wa.me/917012984396;https://i.imgur.com/3cMVKBk.jpeg' ,
